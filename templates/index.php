cc5cbb39dce6196d10c920d44f63fa1bee2d0b4c
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-05 15:16:01 +0800

    getting data from database

--- templates/index.php
+++ templates/index.php
@@ -8,6 +8,20 @@
     if(!$conn){
         echo 'Connection error: ' . mysqli_connect_error();
     }
+
+    // write query for all pizza
+    $sql = 'SELECT title, ingredients, id FROM pizza';
+    //make query and get result
+    $result = mysqli_query($conn, $sql); 
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    //optional but good practice
+    mysqli_free_result($result);
+    //close connection to database
+    mysqli_close($conn);
+
+    print_r($pizzas);
 ?>
 <!DOCTYPE html>
 <html lang="en">
