b967dc6b6ffe1e52389d6dba30fb39d8cb3e1764
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-05 17:31:05 +0800

    rendering data to the browser

--- templates/index.php
+++ templates/index.php
@@ -10,18 +10,18 @@
     }
 
     // write query for all pizza
-    $sql = 'SELECT title, ingredients, id FROM pizza';
+    $sql = 'SELECT title, ingredients, id FROM pizza ORDER BY created_at';
     //make query and get result
     $result = mysqli_query($conn, $sql); 
     // fetch the resulting rows as an array
     $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    //optional but good practice
+    //optional but good practice, free result from memory
     mysqli_free_result($result);
     //close connection to database
     mysqli_close($conn);
 
-    print_r($pizzas);
+    //print_r($pizzas);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,6 +31,31 @@
 
     <?php include('header.php') ; ?>
     
+    <h4 class="center grey-text">Pizzas!</h4>
+
+    <div class="container">
+        <div class="row">
+
+            <?php foreach($pizzas as $pizza){ ?>
+            
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+
+            <?php } ?>
+
+        </div>
+    </div>
+
+
     <?php include('footer.php') ; ?>
 
 </html>
