1b932d621d0d5961d933ae43a17ac2ed4c73a676
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-07 07:37:14 +0800

    deleting a record

--- templates/details.php
+++ templates/details.php
@@ -1,7 +1,22 @@
 <?php 
     
     include('/xampp/htdocs/practice_codes/config/db_connect.php');
-    
+    //detecting that POST request from html below
+    if(isset($_POST['delete'])){
+
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+        $sql = "DELETE FROM pizza WHERE id = $id_to_delete";
+
+        if(mysqli_query($conn, $sql)){
+            //success
+            header('Location: index.php');
+        } else {
+            //fail
+            echo 'query error: ' . mysqli_error($conn);
+        }
+    }
+
     //check GET request id param
     if(isset($_GET['id'])){
         //escape any special character to input in url to db
@@ -38,7 +53,12 @@
             <p><?php echo date($pizza['created_at']) ; ?></p>
             <h5>Ingredients:</h5>
             <p><?php echo htmlspecialchars($pizza['ingredients']) ; ?></p>
-        
+        <!--  delete form -->
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+            </form>
+
         <?php else: ?>
             <h5>No such pizza exists</h5>
         <?php endif; ?>
