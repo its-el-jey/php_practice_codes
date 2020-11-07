b3c4f69b4ddc6e9085168d9e77968958d06afe06
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-07 19:40:24 +0800

    classes&objects part 2

--- classes&objects.php
+++ classes&objects.php
@@ -3,8 +3,8 @@
     class User {
         
         //create property
-        public $email;
-        public $name;
+        private $email;
+        private $name;
 
         //construct
         public function __construct($name, $email){
@@ -15,9 +15,29 @@
         //function
         public function login(){
             //echo 'the user logged in';
-            echo $this->name . 'logged in';
+            echo $this->name . ' logged in';
+        }
+
+        //getters
+        public function getName(){
+            return $this->name;
+        }
+
+        //setters for updating element
+        public function setName($name){
+            if(is_string($name) && strlen($name) > 1){
+                $this->name = $name;
+                return "name has been updated to $name";
+            } else {
+                return 'not a valid name';
+            }
         }
     }
 
     $userTwo = new User('yoshi', ' yoshi@thenetninja.co.uk');
+
+    // echo $userTwo->getName();
+    // echo $userTwo->setName(50);
+    echo $userTwo->setName('shaun');
+    echo $userTwo->getName();
 ?>
