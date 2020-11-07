9d6987e2a8244ffab60dce0a5b1b31d9a893b09a
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-07 20:05:24 +0800

    classes

---
+++ OOP/CLASSES/classess.php
@@ -0,0 +1,23 @@
+<?php 
+
+    class User {
+
+        public $username;
+        public $email;
+
+
+    }
+
+    $userOne = new User();
+    $userTwo = new User();
+
+?>
+
+<html lang="en">
+<head>
+    <title>Class</title>
+</head>
+<body>
+    
+</body>
+</html>
