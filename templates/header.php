8e17a0c45848ad37ef0588b47af021ff26c4cd53
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-07 12:36:13 +0800

    cookies

--- templates/header.php
+++ templates/header.php
@@ -10,6 +10,9 @@
     //you will get error because we are trying to access the $name that doesn't exist anymore because we've unset it
     //so we use NULL COALESCING ?? 'default value'
     $name = $_SESSION['name'] ?? 'Guest';
+
+    //get cookie
+    $gender = $_COOKIE['gender'] ?? 'Unknown';
 ?>
 
 
@@ -44,6 +47,7 @@
             <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
                 <li class="grey-text">Hello <?php echo htmlspecialchars($name) ; ?></li>
+                <li class="grey-text"> (<?php echo htmlspecialchars($gender) ; ?>)</li>
                 <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
             </ul>
         </div>
--- templates/sandbox.php
+++ templates/sandbox.php
@@ -2,6 +2,10 @@
     
     //SUPERGLOBAL sessions
     if(isset($_POST['submit'])){
+
+        //cookie for gender (cookie_name, valued that we apply to that cookie, expiration)
+        setcookie('gender', $_POST['gender'], time() + 86400);
+
         session_start();
 
         $_SESSION['name'] = $_POST['name'];
@@ -23,6 +27,10 @@
 
     <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
     <input type="text" name="name">
+    <select name="gender" id="">
+        <option value="male">male</option>
+        <option value="female">female</option>
+    </select>
     <input type="submit" name="submit" value="submit">
     </form>
 
